d7d2ed953bce2d9d4397df5216ad6279b2b99bb0
Killua <thomas.nguyen3@mail.mcgill.ca>
2023-01-21 13:21:23 -0500

    first component

---
+++ cantcrackthat/src/Password.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Password() {
+    return (
+        <div>
+            <div>
+                THOMAS
+            </div>
+            <div>
+                MARC
+            </div>
+        </div>
+    )
+}
+
+
+export default Password;

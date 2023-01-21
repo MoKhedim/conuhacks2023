a0e717191042cc633e96682fac0fbc607475bcd9
Killua <thomas.nguyen3@mail.mcgill.ca>
2023-01-21 17:40:21 -0500

    algorithm working

--- cantcrackthat/src/Components/Password.js
+++ cantcrackthat/src/Components/Password.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-
+import "../Password.css"
 
 function Password() {
-    const [password, setPassword] = useState("")
+    const [password, setPassword] = useState("");
+    const [passwordShown, setPasswordShown] = useState(false);
+    var zxcvbn = require('zxcvbn');
+    const [crackedTime, setCrackedTime] = useState("");
+    const [score, setScore] = useState("");
+
     function onChangePassword(event) {
         setPassword(event.target.value);
+        //console.log((zxcvbn(event.target.value).score));
+        setCrackedTime("Cracked in " + (zxcvbn(event.target.value).crack_times_display).offline_slow_hashing_1e4_per_second);
+        setScore(zxcvbn(event.target.value).score);
+
+        console.log(score)
+    }
+
+    function viewPassword() {
+        setPasswordShown(!passwordShown);
     }
 
     return (
-        <div>
-            <input type="password" placeholder="Enter Password" onChange={onChangePassword}/>
-            LOL
+        <div className="column" id="passwordContainer">
+            <div id="pass-view">
+                <input className="input is-primary" type={passwordShown ? "text" : "password"} placeholder="Enter Password" onChange={onChangePassword}/>
+                <button onClick={viewPassword} id="view">VIEW</button>
+            </div>
+
+            <div>
+                <p>{password.length == 0 ? crackedTime == "" : crackedTime}</p>
+            </div>
+
+            <div>
+                <progress className="progress is-danger" value={score*25} max="100">15%</progress>
+            </div>
         </div>
     )
 }

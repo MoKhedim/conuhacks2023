710e4493651d1683799423855f148cb0eb86d186
MoKhedim <mohammedkhedim123@gmail.com>
2023-01-21 20:04:21 -0500

    stats

--- cantcrackthat/src/App.js
+++ cantcrackthat/src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import Password from './Components/Main.js';
+import Stats from '/Users/mohammedkhedim/Gitclones/conuhacks2023/cantcrackthat/src/Components/Stats.jsx';
 
 function App() {
   return (
-    <Password />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Stats />
+    </div>
   );
 }
 

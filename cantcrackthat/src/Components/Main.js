70c29942e583648ee5c4143549efa87f363459be
DoBryanCS <Do.Bryan.CS@gmail.com>
2023-01-22 02:06:16 +0100

    zaza

--- cantcrackthat/src/Components/Main.js
+++ cantcrackthat/src/Components/Main.js
@@ -1,25 +1,26 @@
-import React, { useState } from 'react';
-import Password from './Password';
-import PasswordGeneration from './PasswordGeneration';
+import React, { useState } from "react";
+import Password from "./Password";
+import PasswordGeneration from "./PasswordGeneration";
 
 function Main() {
-    const [passOrGen, setPassOrGen] = useState(false);
-    return (
-        <div className="container pt-5">
-            <div className="content">
-                <div className="main-container">
-                    <div className="children-container child-left">
-                        {(!passOrGen) ? <Password setPassOrGen={setPassOrGen} passOrGen={passOrGen} /> : <PasswordGeneration />}
-                    </div>
-                    
-                    <div className="children-container">
-                        <PasswordGeneration />
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+  const [passOrGen, setPassOrGen] = useState(false);
+  return (
+    <div className="container pt-5">
+      <div className="content">
+        <div className="main-container">
+          <div className="children-container child-left">
+            {!passOrGen ? (
+              <Password setPassOrGen={setPassOrGen} passOrGen={passOrGen} />
+            ) : (
+              <PasswordGeneration />
+            )}
+          </div>
 
+          <div className="children-container"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Main;
